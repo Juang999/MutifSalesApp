19e94dc3f0cf9aa43f871c56a25b782b185bd733
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-09-05 15:44:50 +0700

    update helper function

--- helper/Auth.js
+++ helper/Auth.js
@@ -1,20 +1,33 @@
 /**
  * config for Virtual Private Server
 */
-const {parsed: config} = require('dotenv').config({path: '/root/Project/MutifSalesApp/.env'});
+// const {parsed: config} = require('dotenv').config({path: '/root/Project/MutifSalesApp/.env'});
 
 /**
  * config for local WIndows
 */
-// const {parsed: config} = require('dotenv').config({path: 'C:/Users/user/Project/MutifSalesApp/.env'});
+const {parsed: config} = require('dotenv').config({path: 'C:/Users/user/Project/MutifSalesApp/.env'});
 const {get} = require('express-http-context');
 const {verify} = require('jsonwebtoken');
 
 class Auth {
     user = async () => {
-        let {userid, usernama, password, groupid, user_ptnr_id, ptnrg_id} = verify(get('token'), config.ACCESS_TOKEN_SECRET);
+        let token = get('token');
 
-        return {userid, usernama, password, groupid, user_ptnr_id, ptnrg_id};
+        if (token != null) {
+            let {userid, usernama, password, groupid, user_ptnr_id, ptnrg_id} = verify(get('token'), config.ACCESS_TOKEN_SECRET);
+            return {userid, usernama, password, groupid, user_ptnr_id, ptnrg_id};
+        } else {
+            return {
+                userid: null, 
+                usernama: null,
+                password: null, 
+                groupid: null, 
+                user_ptnr_id: null, 
+                ptnrg_id: null
+            }
+        }
+
     }
 }
 
