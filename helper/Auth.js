5d4ad6ce59ca25a4ff0ade9eba9fb02438d4d6b9
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-09-07 11:18:58 +0700

    update config path

--- helper/Auth.js
+++ helper/Auth.js
@@ -1,12 +1,4 @@
-/**
- * config for Virtual Private Server
-*/
-const {parsed: config} = require('dotenv').config({path: '/root/Project/MutifSalesApp/.env'});
-
-/**
- * config for local WIndows
-*/
-// const {parsed: config} = require('dotenv').config({path: 'C:/Users/user/Project/MutifSalesApp/.env'});
+const {config} = require('../config/environment');
 const {get} = require('express-http-context');
 const {verify} = require('jsonwebtoken');
 
@@ -15,7 +7,7 @@ class Auth {
         let token = get('token');
 
         if (token != null) {
-            let {userid, usernama, password, groupid, user_ptnr_id, ptnrg_id} = verify(get('token'), config.ACCESS_TOKEN_SECRET);
+            let {userid, usernama, password, groupid, user_ptnr_id, ptnrg_id} = verify(get('token'), config.parsed.ACCESS_TOKEN_SECRET);
             return {userid, usernama, password, groupid, user_ptnr_id, ptnrg_id};
         } else {
             return {
