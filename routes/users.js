f8018a61c0743f6ce15fd37847d2243041cca0bc
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-09-07 10:13:37 +0700

    update routes

--- routes/users.js
+++ routes/users.js
@@ -1,25 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-/**
- * controller for Virtual Private Server
-*/
-const {loginClient, loginAdmin, getProfile} = require('root/Project/MutifSalesApp/app/controllers/AuthController')
-const AuthMiddleware = require('/root/Project/MutifSalesApp/app/middleware/AuthMiddleware');
-
-/**
- * Controller for local Windows
-*/
-// const {loginClient, loginAdmin, getProfile} = require('C:/Users/user/Project/MutifSalesApp/app/controllers/AuthController')
-// const AuthMiddleware = require('C:/Users/user/Project/MutifSalesApp/app/middleware/AuthMiddleware');
+const {loginClient, loginAdmin, getProfile} = require('../app/controllers/AuthController')
+const AuthMiddleware = require('../app/middleware/AuthMiddleware');
+const AuthRequest = require('../app/requests/AuthRequest');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
-router.post('/login', loginClient);
-router.post('/admin/login', loginAdmin);
+router.post('/login', AuthRequest, loginClient);
+router.post('/admin/login', AuthRequest, loginAdmin);
 router.get('/profile', AuthMiddleware, getProfile);
 
 module.exports = router;
