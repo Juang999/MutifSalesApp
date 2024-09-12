e7540d87ee6ce6017586e704edbe925788274ba3
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2024-09-12 10:54:33 +0700

    update route for updating data chart & update controller

--- routes/Client/sales.js
+++ routes/Client/sales.js
@@ -6,6 +6,6 @@ const {Middleware, Requests} = require('../../app/Kernel')
 router.get('/chart/', [Middleware.AuthMiddleware], getDataChart);
 router.delete('/chart/:cs_oid/delete', [Middleware.AuthMiddleware], deleteChart);
 router.post('/chart/input', [Middleware.AuthMiddleware, Requests.SalesRequests.InputChartRequest], inputIntoChart);
-router.patch('/chart/:cs_oid/update', [Middleware.AuthMiddleware, Requests.SalesRequests.UpdateChartRequest], updateChart);
+router.patch('/chart/update', [Middleware.AuthMiddleware, Requests.SalesRequests.UpdateChartRequest], updateChart);
 
 module.exports = router;
