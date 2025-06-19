95c36b7c34e2b6955d2296a7fc46351e1b7d7597
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2025-06-19 19:28:11 +0700

    update route name

--- routes/Client/productV3.js
+++ routes/Client/productV3.js
@@ -4,9 +4,9 @@ const {index, indexFlashSale, getProductWithGetDescQty, detail, detailFlashSale,
 const {Middleware} = require('../../app/Kernel');
 
 router.get('/', [Middleware.CheckLoginMiddleware], index);
-router.get('/flash-sale', indexFlashSale);
+router.get('/flash-sale-jumbo', indexFlashSale);
 router.get('/:product_code/detail', [Middleware.CheckLoginMiddleware], detail);
-router.get('/:product_code/detail-flash-sale', detailFlashSale);
+router.get('/flash-sale-jumbo/:product_code/detail', detailFlashSale);
 router.get('/getdesc-qty', getProductWithGetDescQty);
 router.get('/getdesc-qty/:product_code/detail', getDetailProductWithGetDescQty);
 
