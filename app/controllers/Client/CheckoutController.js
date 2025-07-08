1696279ef4fe779494d4a4b5c048947df7cebc8e
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2025-07-08 13:37:27 +0700

    update controller

--- app/controllers/Client/CheckoutController.js
+++ app/controllers/Client/CheckoutController.js
@@ -20,8 +20,8 @@ class CheckoutController {
             ])
 
             let dataPartner = null;
-            if (req.body.reference_code) {
-                let resultDataPartner = await PartnerService.getPartnerReference(req.body.reference_code);
+            if (req.body.referral_code) {
+                let resultDataPartner = await PartnerService.getPartnerReference(req.body.referral_code);
 
                 if (resultDataPartner == null) {
                     return {
--- app/requests/SalesRequests/CheckoutRequest.js
+++ app/requests/SalesRequests/CheckoutRequest.js
@@ -7,7 +7,7 @@ const schemaValidate = Joi.object({
     shipping_cost: Joi.number().required(),
     invoice_number: Joi.string().required(),
     is_consigment: Joi.string().required(),
-    reference_code: Joi.string().optional()
+    referral_code: Joi.string().optional()
 })
 
 const CheckoutRequest = (req, res, next) => {
