610ce8cc149d3e8a1069f44fb398ea5103ffbac8
Bangkit Juang Raharjo <juangraharjo03@gmail.com>
2025-10-13 15:36:59 +0700

    update index file

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use('/staff/auth', require('./routes/Staff/auth'));
 app.use('/staff/partner', require('./routes/Staff/partner'));
 app.use('/staff/pricelist', require('./routes/Staff/pricelist'));
 app.use('/staff/sales-quotation', require('./routes/Staff/sales-quotation'));
-app.use('/staff/location', require('./routes/Staff/location')); 
+// app.use('/staff/location', require('./routes/Staff/location')); 
 
 // routes for client
 app.use('/client/register', require('./routes/register'));
